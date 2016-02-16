0762ae30a19ecc3635cb318c352f7714f3ebc548
Denis Denisov <d.denisov@rambler-co.ru>
2016-02-16 13:01:33 +0300

    Goodbye, zsh

--- install.sh
+++ install.sh
@@ -27,10 +27,3 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-
-# zsh
-ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
-ln -s ~/dotfiles/.zsh-update ~/.zsh-update
-ln -s ~/dotfiles/.zsh_history ~/.zsh_history
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zshrc-e ~/.zshrc-e
